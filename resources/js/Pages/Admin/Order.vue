b31d544bd9d4ce1a5616e36f6cfe671867b197b2
chanheinkyaw <chanheinkyaw.mm@gmail.com>
2022-12-31 15:38:49 +0630

    pending success order

---
+++ resources/js/Pages/Admin/Order.vue
@@ -0,0 +1,56 @@
+<template>
+  <Master>
+    <template slot="title">
+        <h1 v-show="!isSuccess">Pending Order</h1>
+        <h1 v-show="isSuccess">Success Order</h1>
+    </template>
+    <template slot="body">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Product</th>
+            <th>Option</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="order in orders.data" :key="order.id">
+            <td>{{ order.user.name }}</td>
+            <td>{{ order.product.name }}</td>
+            <td>
+                <Link :href="`/admin/order/success/${order.id}`" class="badge badge-primary" v-show="!isSuccess">Make Success</Link>
+                <Link :href="`/admin/order/success/${order.id}`" class="badge badge-info" v-show="isSuccess">No Option</Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination :data="orders.links" /> 
+    </template>
+  </Master>
+</template>
+
+<script>
+import Master from "./Master.vue";
+import Pagination from "./Share/Pagination.vue";
+import { Link } from '@inertiajs/inertia-vue';
+
+export default {
+    name: "Order",
+    props: {orders: Object},
+    components:{Master,Link,Pagination},
+    computed: {
+        isSuccess(){
+            if(location.pathname == '/admin/order/success'){
+                return true;
+            }
+            return false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+ .delete-btn{
+  cursor: pointer;
+ }
+</style>
